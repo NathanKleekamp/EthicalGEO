97be3f51a035dd08a92c3dfa2fd01fc98b56505c
Nathan Kleekamp <nkleekamp@gmail.com>
2019-07-30 11:31:46 -0400

    update disqus to ags id

--- js/src/discuss.js
+++ js/src/discuss.js
@@ -24,7 +24,7 @@ var disqus_config = function() {
 
 (function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-s.src = 'https://ethicalgeo.disqus.com/embed.js';
+s.src = 'https://ethicalgeo-1.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
